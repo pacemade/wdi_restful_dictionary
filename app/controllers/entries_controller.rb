16c39838e95e94ffd89e813b371a10d8e66b951a
Danny Chang <dannychachang@gmail.com>
2018-01-11 16:05:39 -0500

    Allow edit to save

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,7 @@
 class EntriesController < ApplicationController
 
   def index
-    @entries = Entry.all
+    @entries = Entry.all.sort
   end
 
   def show
@@ -9,19 +9,39 @@ class EntriesController < ApplicationController
   end
 
   def new
-    render :new
+    @entry = Entry.new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+      # this gives the whole URL
+    else
+      redirect_to new_entry_url
+    end
+
   end
 
   def edit
-    render :edit
+    @entry = Entry.find(params[:id])
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entry_url(params[:id])
+    else
+      redirect_to edit_entry_url(params[:id])
+    end
   end
 
   def destroy
